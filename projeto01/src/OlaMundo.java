c0bea347219747a3a3e1c49cba2905a69570f3fd
engphb <engphb@gmail.com>
2021-02-19 21:49:41 -0300

    Primeiro commit projeto 01

--- projeto01/src/OlaMundo.java
+++ projeto01/src/OlaMundo.java
@@ -1,7 +1,8 @@
 public class OlaMundo {
     //Método executável padrão do java
     public static void main(String[] args) {
-System.out.println("Olá Mundo!");
+System.out.println("Hello World!");
+System.out.println("My name is Francisco");
     }
 
 }
