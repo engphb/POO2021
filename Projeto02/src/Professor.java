e334d62a76a77ac9247b21f257c3363bae1f7f46
engphb <engphb@gmail.com>
2021-03-12 21:10:00 -0300

    Projeto02

--- Projeto02/src/Professor.java
+++ Projeto02/src/Professor.java
@@ -1,4 +1,9 @@
-    public class Professor {
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Professor {
+        Locale ptBr = new Locale("pt", "BR");
         private double salario;
 
         public double getSalario() {
@@ -7,8 +12,14 @@
 
         public void setSalario(double salario) {
             this.salario = salario;
-        }
 
         }
+
+    public String getSalarioFormatado(){
+        DecimalFormat df = new DecimalFormat("###, ###.00");
+        return "R$" + df.format(this.salario);
+    }
+
+}
 
 
